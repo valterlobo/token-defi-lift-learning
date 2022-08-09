2de1b217d9bc4ea8a6ccc4839a49d83b353c6cbb
valter <valterlobo@gmail.com>
2022-08-08 21:31:46 -0300

    feat:tokens ok

--- contracts/WolfSwap.sol
+++ contracts/WolfSwap.sol
@@ -2,21 +2,14 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WolfSwap is Pausable, AccessControl {
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
-    constructor() {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
-    }
-
-    function pause() public onlyRole(PAUSER_ROLE) {
+contract WolfSwap is Pausable, Ownable {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyOwner {
         _unpause();
     }
 }
