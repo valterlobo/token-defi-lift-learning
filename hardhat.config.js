df55cae7973e94db6d732a4383c0a65025a42bf5
valter <valterlobo@gmail.com>
2022-08-09 01:25:25 -0300

    save

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,29 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+require('@nomiclabs/hardhat-solhint');
+require('solidity-coverage');
+require('dotenv').config();
+
+const { WALLET_PRIVATE_KEY } = process.env;
+const { POLYGONSCAN_API_KEY } = process.env;
+const { INFURA_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    matic: {
+      url: INFURA_URL,
+      accounts: [`0x${WALLET_PRIVATE_KEY}`],
+    },
+    localhost: {
+      url: process.env.LOCALHOST_URL || '',
+      accounts:
+        process.env.LOCAL_PRIVATE_KEY !== undefined
+          ? [process.env.LOCAL_PRIVATE_KEY]
+          : [],
+    },
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY,
+  },
 };
